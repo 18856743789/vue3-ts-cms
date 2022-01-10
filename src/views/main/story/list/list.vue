cdce9418469b8bdd4b9f9f52d72def64f6e309c3
YJzhangyh <zhangyh@yunjiacloud.com>
2022-01-10 10:56:30 +0800

    feat: 富文本编辑器的使用

--- src/views/main/story/list/list.vue
+++ src/views/main/story/list/list.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="list">
-    <h2>list</h2>
+    <page-content
+      :contentTableConfig="contentTableConfig"
+      pageName="story"
+    ></page-content>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+import PageContent from '@/components/page-content'
+import { contentTableConfig } from './config/content.config'
+
 export default defineComponent({
   name: 'list',
+  components: {
+    PageContent
+  },
   setup() {
-    return {}
+    return {
+      contentTableConfig
+    }
   }
 })
 </script>
